0ce34bf663037cead2611db75c006c9f68dbb855
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2015-06-19 23:49:36 -0400

    Remove exercise files
    This app won't need exercise files as we concentrate on finishing final
    exercise.

--- node-exercises/params/app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/team/:name', function(req, res){
-  res.setHeader('Content-Type','text/plain');
-  res.send("You picked " + req.params.name);
-});
-
-var server = app.listen(8080);
--- node-exercises/static-assets/public/kitties.js
+++
@@ -1,5 +0,0 @@
-$('button').click(function() {
-  var x = Math.round(Math.random() * 500);
-  var y = Math.round(Math.random() * 500);
-  $('div').append('<img src="http://placekitten.com/' + x + '/' + y + '">');
-});
